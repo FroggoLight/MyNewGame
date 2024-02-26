8005b29dc0e6a0dbb63b6afce1e96087ef0c9f70
FroggoLight <ineedsomefriendsss@gmail.com>
2024-02-25 19:29:12 -0500

    Added playable notes for testing

--- function.js
+++ function.js
@@ -1,9 +1,20 @@
-let validKeys = ["v", "b", "n", "m"];
+const validKeys = ["z", "x", "c", "v"];
 
 const columns = Array.from(document.querySelectorAll(".NoteColumn"));
-console.log(columns); 
-console.log(columns[1]);
-//columns[1].style.background = "linear-gradient(0deg, rgba(8,141,177,1) 0%, rgba(38,38,38,1) 10%)";
+
+const rootVar = getComputedStyle(document.querySelector(":root"));
+
+const tempChartData = [[1, 1, 0, 0],
+                       [0, 0, 0, 0], 
+                       [1, 1, 0, 0],
+                       [0, 0, 0, 0],
+                       [1, 1, 0, 1],
+                       [0, 0, 1, 0], 
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0],
+                       [1, 1, 0, 1]];
+
+
 
 window.addEventListener("keydown", function (event) {
     let keyPressed = event.key;
@@ -12,7 +23,10 @@ window.addEventListener("keydown", function (event) {
             return; //damn im so smart from using google
         } 
         let keyIndex = validKeys.indexOf(keyPressed);
-        columns[keyIndex].style.background = "linear-gradient(0deg, rgba(8,141,177,1) 0%, rgba(38,38,38,1) 10%)";
+        columns[keyIndex].style.background = "rgba(55,55,55,1)";
+        if (columns[keyIndex].firstChild != null) {
+            processHit(columns[keyIndex]);
+        }
         //on the press see if there are any notes to judge
         //console.log("a proper key was hit");
     }
@@ -29,4 +43,34 @@ window.addEventListener("keyup", function (event) {
         //console.log("a proper key was hit");
     }
 })
+
+function processHit(lane) {
+    lane.removeChild(lane.firstChild);
+}
+
+function generateLaneNotes(chartData) {
+    let delayMultiplier = 0.08;
+    for (i = 0; i < chartData.length; i++) {
+        for (j = 0; j < 4; j++) {
+            if (chartData[i][j] == 1) {
+                let tempNote = document.createElement("div");
+                tempNote.classList.add("notes");
+                tempNote.style.animationName = "moveDown";
+                tempNote.style.animationTimingFunction = "linear";
+                tempNote.style.animationDuration = "0.8s";
+                tempNote.style.animationDelay = delayMultiplier * i + "s"; // for debugging
+                tempNote.style.opacity = 0;
+                tempNote.style.animationPlayState = "running";
+
+                columns[j].appendChild(tempNote);
+            }
+        }
+    }
+}
+
+
+
+generateLaneNotes(tempChartData);
+//if a note is not within a certain ms of window (aka ms * distance between the perfect judge), then do nothing
+//else, we perform a judgement on the note
 
